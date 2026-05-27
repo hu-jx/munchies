bd8b3b1ec3820671b61a27986cd60071ee6b337f
hu-jx <jiaxin.hu1608@gmail.com>
2026-05-27 16:26:39 +0800

    bugFix: fix error in server.js

--- server/server.js
+++ server/server.js
@@ -3,14 +3,15 @@ import dotenv, { config } from 'dotenv'
 config({quiet: true})
 import express, { json } from 'express'
 import {connectDB} from './config/db.js'
-import auth_routes from './routes/auth_routes.js'
+import router from './routes/auth_routes.js'
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 //Configuration
 app.use(json())
-app.use('/api', auth_routes)
+app.use('/api', router)
+
 const startServer = async () => {
     await connectDB()
     app.listen(PORT, () => console.log('Server runnning on', PORT))
