f1931b19375958da4fc590dd07fa29726e59c9a7
hu-jx <jiaxin.hu1608@gmail.com>
2026-06-14 15:24:20 +0800

    fix(multiple-redirection): resolve issue on multiple re-navigation when buttons are spam-clicked

--- frontend_munchies/lib/widgets/button.dart
+++ frontend_munchies/lib/widgets/button.dart
@@ -1,26 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:frontend_munchies/styles/colours.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:tap_debouncer/tap_debouncer.dart';
 
-class AppButton extends StatelessWidget {
+class AppButton extends StatefulWidget {
   final String text;
-  final VoidCallback onPressed;
+  final Future<void> Function() onPressed;
+  final Size size;
 
-  const AppButton({super.key, required this.text, required this.onPressed});
+  const AppButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    required this.size,
+  });
 
   @override
+  State<AppButton> createState() => _AppButtonState();
+}
+
+class _AppButtonState extends State<AppButton> {
+  @override
   Widget build(BuildContext context) {
-    return OutlinedButton(
-      onPressed: onPressed,
-      style: OutlinedButton.styleFrom(
-        backgroundColor: Colours.greyPink,
-        fixedSize: Size(298, 48),
-        side: BorderSide(color: Colours.greyPink),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      ),
-      child: Text(
-        text,
-        style: GoogleFonts.poppins(color: Colors.white, fontSize: 16.0),
+    return TapDebouncer(
+      onTap: () async => await widget.onPressed(),
+      builder: (BuildContext context, TapDebouncerFunc? onTap) => OutlinedButton(
+        onPressed: onTap,
+        style: OutlinedButton.styleFrom(
+          backgroundColor: Colours.greyPink,
+          fixedSize: widget.size,
+          side: BorderSide(color: Colours.greyPink),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        ),
+        child: Text(
+          widget.text,
+          style: GoogleFonts.poppins(color: Colors.white, fontSize: 16.0),
+        ),
       ),
     );
   }
