f26065cdd4551e0cb3110979239cbe7c8896f1a0
t3cia <teoqn.tricia@gmail.com>
2026-06-17 22:41:38 +0800

    feat: added friends field to userSchema, and updated dashboardFeature ui

--- server/models/user.js
+++ server/models/user.js
@@ -29,7 +29,13 @@ const userSchema = new Schema({
         required: false,
         default: null,
         trim: true
-    }
+    },
+
+    friends: {
+        type: String,
+        required: false,
+        default: []
+    },
 },
     {
         timestamps: true
