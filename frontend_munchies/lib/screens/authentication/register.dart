08de11bef75bd7b8deace81a6895d5ab56a95576
hu-jx <jiaxin.hu1608@gmail.com>
2026-06-27 19:24:09 +0800

    test: login and register tests creaeted and passed along with minor refactoring

--- frontend_munchies/lib/screens/authentication/register.dart
+++ frontend_munchies/lib/screens/authentication/register.dart
@@ -10,13 +10,15 @@ import 'package:frontend_munchies/screens/main_screen.dart';
 // Use GoogleFonts.font_family to obtain desired font (e.g. GoogleFonts.poppins)
 
 class RegisterPage extends StatefulWidget {
+  final Authentication authentication;
+  final Homepage? homepage;
+  RegisterPage({super.key, required this.authentication, this.homepage});
+
   final textStyle = GoogleFonts.poppins(
     color: Colours.greyPink,
     decorationColor: Colours.greyPink,
     fontSize: 16.0,
   );
-
-  RegisterPage({super.key});
 
   @override
   State<RegisterPage> createState() => _RegisterPageState();
@@ -78,123 +80,128 @@ class _RegisterPageState extends State<RegisterPage> {
   }
 
   Widget buildCenter() {
-    return Form(
-      key: formKey,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(height: 10.0),
-          Center(
-            child: Text(
-              'MUNCHIES',
-              style: GoogleFonts.cherryBombOne(
-                color: Colours.greyPink,
-                fontSize: 52.0,
-              ),
-            ),
-          ),
-          Center(
-            child: Text(
-              'Create an account with munchies',
-              style: widget.textStyle,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 10, left: 30, bottom: 5),
-            child: Text('Enter Email Address *', style: widget.textStyle),
-          ),
-          GeneralTextfield(
-            controller: emailController,
-            labelText: 'Email Address',
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 10, left: 30, bottom: 5),
-            child: Text('Confirm Email Address *', style: widget.textStyle),
-          ),
-          GeneralTextfield(
-            controller: confirmEmailController,
-            labelText: 'Confirm Email Address',
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return "Field cannot be empty.";
-              }
-              if (emailController.text != confirmEmailController.text) {
-                return "Email did not match";
-              }
-              return null;
-            },
-          ),
-          buildNameRow(),
-          Padding(
-            padding: const EdgeInsets.only(top: 10, left: 30, bottom: 5),
-            child: Text('Enter Password *', style: widget.textStyle),
-          ),
-          PasswordTextfield(pwController: pwController, labelText: 'Password'),
-          Padding(
-            padding: const EdgeInsets.only(top: 10, left: 30, bottom: 5),
-            child: Text('Confirm Password *', style: widget.textStyle),
-          ),
-          PasswordTextfield(
-            pwController: confirmPwController,
-            labelText: 'Confirm Password',
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return "Field cannot be empty.";
-              }
-              if (pwController.text != confirmPwController.text) {
-                return "Password did not match";
-              }
-              return null;
-            },
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 5, left: 30, bottom: 10),
-            child: Text(
-              'Fields marked * are compulsory',
-              style: GoogleFonts.poppins(color: Colours.grey),
-            ),
-          ),
-          Center(
-            child: AppButton(
-              text: 'Sign up now!',
-              onPressed: () async {
-                if (formKey.currentState?.validate() == true) {
-                  await tryRegister();
-                }
-              },
-              size: Size(298, 48),
-            ),
-          ),
-          Row(
+    return ScrollConfiguration(
+      behavior: ScrollBehavior().copyWith(overscroll: false),
+      child: SingleChildScrollView(
+        child: Form(
+          key: formKey,
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                'Already have an account?',
-                style: GoogleFonts.poppins(color: Colours.grey),
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
+              SizedBox(height: 10.0),
+              Center(
                 child: Text(
-                  'Login now!',
-                  style: GoogleFonts.poppins(
+                  'MUNCHIES',
+                  style: GoogleFonts.cherryBombOne(
                     color: Colours.greyPink,
-                    decorationColor: Colours.greyPink,
-                    decoration: TextDecoration.underline,
+                    fontSize: 52.0,
                   ),
+                ),
+              ),
+              Center(
+                child: Text(
+                  'Create an account with munchies',
+                  style: widget.textStyle,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10, left: 30, bottom: 5),
+                child: Text('Enter Email Address *', style: widget.textStyle),
+              ),
+              GeneralTextfield(
+                controller: emailController,
+                labelText: 'Email Address',
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10, left: 30, bottom: 5),
+                child: Text('Confirm Email Address *', style: widget.textStyle),
+              ),
+              GeneralTextfield(
+                controller: confirmEmailController,
+                labelText: 'Confirm Email Address',
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Field cannot be empty.";
+                  }
+                  if (emailController.text != confirmEmailController.text) {
+                    return "Email did not match";
+                  }
+                  return null;
+                },
+              ),
+              buildNameRow(),
+              Padding(
+                padding: const EdgeInsets.only(top: 10, left: 30, bottom: 5),
+                child: Text('Enter Password *', style: widget.textStyle),
+              ),
+              PasswordTextfield(pwController: pwController, labelText: 'Password'),
+              Padding(
+                padding: const EdgeInsets.only(top: 10, left: 30, bottom: 5),
+                child: Text('Confirm Password *', style: widget.textStyle),
+              ),
+              PasswordTextfield(
+                pwController: confirmPwController,
+                labelText: 'Confirm Password',
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Field cannot be empty.";
+                  }
+                  if (pwController.text != confirmPwController.text) {
+                    return "Password did not match";
+                  }
+                  return null;
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 5, left: 30, bottom: 10),
+                child: Text(
+                  'Fields marked * are compulsory',
+                  style: GoogleFonts.poppins(color: Colours.grey),
+                ),
+              ),
+              Center(
+                child: AppButton(
+                  text: 'Sign up now!',
+                  onPressed: () async {
+                    if (formKey.currentState?.validate() == true) {
+                      await tryRegister();
+                    }
+                  },
+                  size: Size(298, 48),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Already have an account?',
+                    style: GoogleFonts.poppins(color: Colours.grey),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      'Login now!',
+                      style: GoogleFonts.poppins(
+                        color: Colours.greyPink,
+                        decorationColor: Colours.greyPink,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Center(
+                child: Text(
+                  errorMessage ?? "",
+                  style: GoogleFonts.poppins(color: Colors.red, fontSize: 14.0),
                 ),
               ),
             ],
           ),
-          Center(
-            child: Text(
-              errorMessage ?? "",
-              style: GoogleFonts.poppins(color: Colors.red, fontSize: 14.0),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
@@ -281,7 +288,7 @@ class _RegisterPageState extends State<RegisterPage> {
 
   Future<void> tryRegister() async {
     try {
-      await Authentication().register(
+      await widget.authentication.register(
         emailController.text,
         pwController.text,
         fNameController.text,
@@ -293,7 +300,8 @@ class _RegisterPageState extends State<RegisterPage> {
       });
       Navigator.push(
         context,
-        MaterialPageRoute(builder: (context) => Homepage(), settings: RouteSettings(name: '/home')),
+        MaterialPageRoute(builder: (context) => widget.homepage ?? Homepage(),
+         settings: RouteSettings(name: '/home')),
       );
     } catch (e) {
       setState(() {
---
+++ frontend_munchies/test/widget-tests/auth/register_view_test.dart
@@ -0,0 +1,136 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:frontend_munchies/models/user_profile.dart';
+import 'package:frontend_munchies/screens/activities/domain/repositories/record_repo.dart';
+import 'package:frontend_munchies/screens/authentication/register.dart';
+import 'package:frontend_munchies/screens/main_screen.dart';
+import 'package:frontend_munchies/services/auth/auth_services_repo.dart';
+import 'package:frontend_munchies/services/auth/authentication.dart';
+import 'package:frontend_munchies/widgets/button.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:provider/provider.dart';
+
+import '../../mocks/mock_navi_observer.dart';
+
+//view test
+class MockFirebaseAuth extends Mock implements FirebaseAuth {}
+class MockAuthService extends Mock implements AuthServicesRepo {}
+class MockRecordRepo extends Mock implements RecordRepository {}
+class MockUserCredential extends Mock implements UserCredential {}
+class MockUser extends Mock implements User {}
+void main() {
+  late UserProfile mockProfile;
+  late Authentication mockAuth;
+  late MockFirebaseAuth mockFirebaseAuth;
+  late MockAuthService mockAuthService;
+  late MockNaviObserver mockNaviObserver;
+  late MockRecordRepo mockRecordRepo;
+  late MockUserCredential userCredential;
+  late MockUser user;
+
+  setUpAll(() {
+    mockProfile = UserProfile(firebase_uid: 'id', emailAddress: 'new@gmail.com', password: 'password', firstName: 'mock');
+    registerFallbackValue(mockProfile);
+  });
+
+  setUp(() {
+    // streamController = StreamController.broadcast();
+    // filter = ActivityFilter.all;
+    userCredential = MockUserCredential();
+    user = MockUser();
+    mockRecordRepo = MockRecordRepo();
+    mockNaviObserver = MockNaviObserver();
+    mockFirebaseAuth = MockFirebaseAuth();
+    mockAuthService = MockAuthService();
+    mockAuth = Authentication(firebaseAuth: mockFirebaseAuth, apiServices: mockAuthService);
+
+    //stub get user method in mockusercred
+    when(() => userCredential.user).thenReturn(user);
+    //stub user method 
+    when(() => user.getIdToken()).thenAnswer((_) async => 'idToken');
+    when(() => user.uid).thenReturn('uid');
+
+    //stub fetchUserProfile method
+    when(() => mockAuthService.createProfile(any<String>(), any<UserProfile>())).thenAnswer((inv) async {
+      debugPrint('${inv.positionalArguments}');
+      return Future(() => null); 
+    });
+    //stub signInWith.... method
+    when(() => mockFirebaseAuth.createUserWithEmailAndPassword(email: any<String>(named: 'email'), password: any<String>(named: 'password')))
+    .thenAnswer((inv) async {
+      debugPrint('called createUser');
+      return userCredential;
+    }
+    );
+  });
+
+  Widget createRegisterPage({List<NavigatorObserver> naviObs = const []}) {
+    return Provider<RecordRepository>.value(value: mockRecordRepo, 
+    child: MaterialApp(
+      navigatorObservers: naviObs,
+      home: Scaffold(body: RegisterPage(authentication: mockAuth, 
+      homepage: Homepage(viewOptions: [
+        const Placeholder(),
+        const Placeholder(),
+        const Placeholder(),
+        const Placeholder(),
+        const Placeholder(),
+      ],),)),
+    ),);
+  }
+
+  Future<void> loadRegisterPage(WidgetTester tester) async {
+    await tester.pumpWidget(createRegisterPage(naviObs: [mockNaviObserver]));
+    await tester.pumpAndSettle();
+  }
+
+  Future<void> keyInValue(Finder finder, String text, WidgetTester tester) async {
+    await tester.enterText(finder, text);
+    await tester.pumpAndSettle();
+    await tester.tapAt(Offset.zero);
+    await tester.pumpAndSettle();
+  }
+
+  testWidgets('set up', (tester) async {
+    await loadRegisterPage(tester);
+    expect(find.byType(RegisterPage), findsOne);
+  });
+  testWidgets('Key in the correct login values and user is navigated to their activities homepage', (tester) async {
+    await loadRegisterPage(tester);
+    debugPrint('here');
+    //stub the signIn method
+    await keyInValue(find.widgetWithText(TextFormField, 'Email Address'), 'new@gmail.com', tester);
+    debugPrint('here1');
+    await keyInValue(find.widgetWithText(TextFormField, 'Confirm Email Address'), 'new@gmail.com', tester);
+    debugPrint('here2');
+    await keyInValue(find.widgetWithText(TextFormField, 'First Name'), 'mock', tester);
+    debugPrint('here3');
+    await keyInValue(find.widgetWithText(TextFormField, 'Password'), 'password', tester);
+    debugPrint('her');
+    await keyInValue(find.widgetWithText(TextFormField, 'Confirm Password'), 'password', tester);
+    debugPrint('here5');
+
+    //scroll until visible 
+    await tester.scrollUntilVisible(find.byType(AppButton), 200.0, scrollable: find.byType(Scrollable).last);
+    expect(find.text('Sign up now!'), findsOne);
+    await tester.tap(find.text('Sign up now!'));
+    await tester.pumpAndSettle();
+
+    verify(() => mockFirebaseAuth.createUserWithEmailAndPassword(email: 'new@gmail.com', password: 'password'),).called(1);
+    verify(() => mockAuthService.createProfile(any(), any())).called(1);
+    expect(find.byType(Homepage), findsOne);
+    expect(mockNaviObserver.pushed.last.settings.name, '/home');
+  });
+
+  testWidgets('Key in wrong values and trigger validator', (tester) async {
+    await loadRegisterPage(tester);
+
+    await tester.scrollUntilVisible(find.byType(AppButton), 200.0, scrollable: find.byType(Scrollable).last);
+    expect(find.text('Sign up now!'), findsOne);
+    await tester.tap(find.text('Sign up now!'));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Field cannot be empty.'), findsNWidgets(5));
+  });
+}
